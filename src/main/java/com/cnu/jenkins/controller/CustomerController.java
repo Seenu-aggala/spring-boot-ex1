d206218ec2d1d2408fb076bb87b84eae3a7097bf
Seenu-aggala <80087225+Seenu-aggala@users.noreply.github.com>
2021-11-23 09:35:26 +0530

    Update CustomerController.java

--- src/main/java/com/cnu/jenkins/controller/CustomerController.java
+++ src/main/java/com/cnu/jenkins/controller/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerController {
 	
-	@GetMapping("/hello")
+	@GetMapping("/jenkin")
 	private String helloMethod(@RequestParam String name) {
 		return "Hello world..."+name;
 	}
